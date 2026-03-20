110e5a86194197946c3969544a8a8ba2f690e88a
Benjamin-A-L-duocuc <ben.astudillol@duocuc.cl>
2026-03-20 14:20:46 -0300

    add class libro

---
+++ bibliotecaduoc/src/main/java/com/example/bibliotecaduoc/model/libro.java
@@ -0,0 +1,10 @@
+package com.example.bibliotecaduoc.model;
+
+public class libro {
+    private int id;
+    private String isbn;
+    private String titulo;
+    private String editorial;
+    private String fechaPublicacion;
+    private String autor;
+}
